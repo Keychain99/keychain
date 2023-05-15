738bf1f1fa89690df3d2b6b186e9def55c2affa3
Keychain99 <129546469+Keychain99@users.noreply.github.com>
2023-05-15 15:51:30 +0300

    Add files via upload

--- keychain.js
+++ keychain.js
@@ -1,14 +1,14 @@
 const modal = document.getElementById('modal');
 const modalActivate = document.getElementById('modal__activate')
-const closeBtn = document.getElementById('close')
+const closeBtn = document.getElementById('close__btn')
 
 const modalActive = () => {
     modal.style.display = 'block'
 }
 
-// const closeWindow = () => {
-//     modal.style.display = 'none'
-// }
+const closeWindow = () => {
+    modal.style.display = 'none'
+}
 
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -16,5 +16,5 @@ window.onclick = function(event) {
     }
   }
 
-// closeBtn.addEventListener('click',closeWindow)
+closeBtn.addEventListener('click',closeWindow)
 modalActivate.addEventListener('click',modalActive)
